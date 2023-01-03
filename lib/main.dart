a5dd5b7f036fd813709e9e521514406a9a319e28
Anirban Basak <jeetbasak2002@gmail.com>
2023-01-04 04:45:23 +0530

    completed (04/01/2023)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -6,18 +7,75 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: const Text('Dice'),
           backgroundColor: Colors.red,
         ),
-        body: DicePage(),
+        body: const DicePage(),
       ),
     ),
   );
 }
+/// This is a Stateful Widget which can change state like changing Images or texts or any
+/// UI resources and these can't be accomplished normally by a stateless Widget which does
+/// not allow us to change the State.
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
 
-class DicePage extends StatelessWidget {
+  // Passing the State of the Widget to its Parent Stateful Widget and linking the state to it.
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+/// It is the state of the Above widget which basically holds all the stateful work like changing
+/// the UI accordingly
+class _DicePageState extends State<DicePage> {
+
+  // Declaring the dice Count for left and right Dice
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 2;
+
+  // This function sets the New Dice variables to change the dice Images
+  void setDiceNumbers() {
+
+    // SetState method calls the build method again and draws all the changes done
+    setState(() {
+
+      // Random().nextInt(6) gives a random number between 0 to 5
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          // It Expands the Image to take the whole Screen
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextButton(
+                onPressed: () {
+                  setDiceNumbers();
+                },
+                child: Image.asset('images/dice$leftDiceNumber.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextButton(
+                onPressed: () {
+                  setDiceNumbers();
+                },
+                child: Image.asset('images/dice$rightDiceNumber.png'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
